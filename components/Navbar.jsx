1b6454cb1dc723a2978d83f13be1f0db1354b5fb
mikelrisyad <mikelrisyad@gmail.com>
2022-01-20 09:16:21 +0700

    fix: edit navbar color and text

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,60 +1,52 @@
 const Navbar = () => {
   return (
     <>
-      <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-        <div className="flex items-center flex-shrink-0 text-white mr-6">
-          <svg
-            className="fill-current h-8 w-8 mr-2"
-            width="54"
-            height="54"
-            viewBox="0 0 54 54"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-          </svg>
-          <span className="font-semibold text-xl tracking-tight">
-            Tailwind CSS
+      <nav className="flex items-center justify-between flex-wrap p-8">
+        <div className="flex items-center flex-shrink-0  mr-6">
+          <span className="font-semibold text-3xl tracking-tight text-ruddy-pink">
+            LepasAja
           </span>
         </div>
-        <div className="block lg:hidden">
-          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-            <svg
-              className="fill-current h-3 w-3"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
-        </div>
-        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-          <div className="text-sm lg:flex-grow">
+
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto text-center">
+          <div className="text-sm lg:flex-grow text-ruddy-pink">
             <a
               href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+              className="block mt-4 lg:inline-block lg:mt-0 hover:underline underline-offset-8 text-lg mr-4"
             >
-              Docs
+              Home
             </a>
             <a
               href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+              className="block mt-4 lg:inline-block lg:mt-0 hover:underline underline-offset-8 text-lg mr-4"
             >
-              Examples
+              About
             </a>
             <a
               href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+              className="block mt-4 lg:inline-block lg:mt-0 hover:underline underline-offset-8 text-lg mr-4"
             >
               Blog
             </a>
+            <a
+              href="#responsive-header"
+              className="block mt-4 lg:inline-block lg:mt-0 hover:underline underline-offset-8 text-lg"
+            >
+              Know How
+            </a>
           </div>
-          <div>
+          <div className="space-x-2">
             <a
               href="#"
-              className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+              className="inline-block text-sm px-4 py-2 leading-none border rounded bg-ruddy-pink text-white border-white hover:border-ruddy-pink hover:text-ruddy-pink hover:bg-white mt-4 lg:mt-0"
             >
-              Download
+              Login
+            </a>
+            <a
+              href="#"
+              className="inline-block text-sm px-4 py-2 leading-none border rounded text-ruddy-pink border-ruddy-pink hover:border-transparent hover:text-white hover:bg-ruddy-pink mt-4 lg:mt-0"
+            >
+              Sign Up
             </a>
           </div>
         </div>
