0db3e77be6b604b86f6267d6eca8af327aa4a2a2
mikelrisyad <mikelrisyad@gmail.com>
2022-01-20 23:32:20 +0700

    feat: add footer on layout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,11 +1,13 @@
 import styles from '../styles/Home.module.css';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
     <div className={styles.container}>
       <Navbar />
       {children}
+      <Footer />
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -17,19 +17,6 @@ export default function Home() {
         </h1>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 }
