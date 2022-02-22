7400e94720666ecdfb6f57257d269b84b9b57503
dharmatriyasa <dharmatriyasa19@gmail.com>
2022-02-23 00:10:51 +0800

    fix: flow reset password firebase

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import { 
+//   getAuth,
+//   sendPasswordResetEmail
+// } 
+// from "firebase/auth";
+// import firebase from '../config/firebase.config.js';
+
+// const auth = getAuth(firebase);
+// console.log(auth);
 
 export default function Modal(props) {
+  const [email, setEmail] = useState('');
+
+  function onSendEmailSubmit(e){
+    e.preventDefault();
+
+    return props.onSendEmail({ email })
+  }
+
+  
+
+  
   return (
     <div className="fixed top-0 bottom-0 left-0 right-0 z-10 flex items-center justify-center w-screen h-screen p-4 overflow-x-hidden overflow-y-auto">
       <div
@@ -9,13 +29,22 @@ export default function Modal(props) {
       ></div>
       <div className="rounded-md flex flex-col items-center relative max-w-[720px] w-full py-14 px-4 shadow-md bg-ruddy-pink">
         <h2 className="text-lg text-white">EMAIL</h2>
-        <input
-          className="max-w-[500px] w-full mt-3 mb-5 bg-white rounded-md focus:outline-none focus:shadow-outline text-[#DF8D9F] text-base p-2"
-          placeholder="Enter your email here"
-        />
-        <button className="px-6 py-2 font-medium bg-white text-ruddy-pink rounded-[4px]">
+        <form className='flex flex-col justify-center items-center  w-full max-w-[500px]' onSubmit={onSendEmailSubmit}>
+          <input
+            className="max-w-[500px] w-full mt-3 mb-5 bg-white rounded-md focus:outline-none focus:shadow-outline text-[#DF8D9F] text-base p-2"
+            placeholder="Enter your email here"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <input 
+            className="w-24 px-6 py-2 font-medium bg-white text-ruddy-pink rounded-[4px]"
+            type="submit"
+            value={`SEND`} 
+          />
+        </form>
+        {/* <button className="px-6 py-2 font-medium bg-white text-ruddy-pink rounded-[4px]">
           SEND
-        </button>
+        </button> */}
       </div>
     </div>
   );
--- config/firebase.config.js
+++ config/firebase.config.js
@@ -9,7 +9,18 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+
+  // apiKey: 'AIzaSyBi2tNgLucv8amFPp9vCXJcM03kITvXtc4',
+  // authDomain: 'lepas-aja.firebaseapp.com',
+  // projectId: 'lepas-aja',
+  // storageBucket: 'lepas-aja.appspot.com',
+  // messagingSenderId: '472438318678',
+  // appId: '1:472438318678:web:4ae14c27206c60238afdf7',
+  // measurementId: 'G-79MSWQG7JC',
 };
+
+// console.log(firebaseConfig);
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
