6a4a010efd53244acdbbf707a4e3bca22a213894
moehzi <faizi1234098@gmail.com>
2022-02-24 00:01:57 +0800

    refactor: move request api to file api.js

--- services/api.js
+++ services/api.js
@@ -7,3 +7,19 @@ export const getProfileById = async (id, state) => {
   let biodata = await response.data;
   state(biodata.data);
 };
+
+export async function fetchProvinsi(state) {
+  let response = await axios(
+    `https://dev.farizdotid.com/api/daerahindonesia/provinsi`
+  );
+  let dataProvinsi = await response.data;
+  state(dataProvinsi.provinsi);
+}
+
+export async function fetchKota(state, idKota) {
+  let response = await axios(
+    `https://dev.farizdotid.com/api/daerahindonesia/kota?id_provinsi=${idKota}`
+  );
+  let dataKota = await response.data;
+  state(dataKota.kota_kabupaten);
+}
