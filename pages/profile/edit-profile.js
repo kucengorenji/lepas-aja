82c8e55ac90a52b7f8f78f1cefb63b3f93c493da
moehzi <faizi1234098@gmail.com>
2022-02-24 00:01:57 +0800

    refactor: move request api to file api.js

--- pages/profile/edit-profile.js
+++ pages/profile/edit-profile.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import axios from 'axios';
+import { fetchProvinsi, fetchKota } from '../../services/api';
 
 const EditProfile = () => {
   const [startDate, setStartDate] = useState(new Date());
@@ -12,25 +13,9 @@ const EditProfile = () => {
   const [kota, setKota] = useState('');
   const [idKota, setIdKota] = useState();
 
-  async function fetchProvinsi() {
-    let response = await axios(
-      `https://dev.farizdotid.com/api/daerahindonesia/provinsi`
-    );
-    let dataProvinsi = await response.data;
-    setDataProvinsi(dataProvinsi.provinsi);
-  }
-
-  async function fetchKota() {
-    let response = await axios(
-      `https://dev.farizdotid.com/api/daerahindonesia/kota?id_provinsi=${idKota}`
-    );
-    let dataKota = await response.data;
-    setDataKota(dataKota.kota_kabupaten);
-  }
-
   useEffect(() => {
-    fetchProvinsi();
-    fetchKota();
+    fetchProvinsi(setDataProvinsi);
+    fetchKota(setDataKota, idKota);
   }, [idKota]);
 
   return (
@@ -143,10 +128,10 @@ const EditProfile = () => {
               <label className="text-2xl font-medium opacity-70">
                 Foto Profile
               </label>
-              <div class="flex justify-center px-6 pt-5 pb-6 mt-1 border-2 border-gray-300 border-dashed rounded-md">
-                <div class="space-y-1 text-center">
+              <div className="flex justify-center px-6 pt-5 pb-6 mt-1 border-2 border-gray-300 border-dashed rounded-md">
+                <div className="space-y-1 text-center">
                   <svg
-                    class="w-12 h-12 mx-auto text-gray-400"
+                    className="w-12 h-12 mx-auto text-gray-400"
                     stroke="currentColor"
                     fill="none"
                     viewBox="0 0 48 48"
@@ -154,27 +139,29 @@ const EditProfile = () => {
                   >
                     <path
                       d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     />
                   </svg>
-                  <div class="flex text-sm text-gray-600">
+                  <div className="flex text-sm text-gray-600">
                     <label
-                      for="file-upload"
-                      class="relative font-medium text-indigo-600 bg-white rounded-md cursor-pointer hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                      htmlFor="file-upload"
+                      className="relative font-medium text-indigo-600 bg-white rounded-md cursor-pointer hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
                     >
                       <span>Upload a file</span>
                       <input
                         id="file-upload"
                         name="file-upload"
                         type="file"
-                        class="sr-only"
+                        className="sr-only"
                       />
                     </label>
-                    <p class="pl-1">or drag and drop</p>
+                    <p className="pl-1">or drag and drop</p>
                   </div>
-                  <p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+                  <p className="text-xs text-gray-500">
+                    PNG, JPG, GIF up to 10MB
+                  </p>
                 </div>
               </div>
             </div>
