7a2f3309cd8777daf18f1a763fba27a8b7542137
mikelrisyad <mikelrisyad@gmail.com>
2022-02-24 06:03:28 +0700

    style: edit transition

--- components/CardProduct.jsx
+++ components/CardProduct.jsx
@@ -11,7 +11,7 @@ export default function CardProduct(props) {
             alt={props.name}
             className="object-cover w-full rounded-t-lg h-60"
           />
-          <div className="invisible group-hover:visible absolute bottom-0 left-0 w-full px-6 py-4 bg-ruddy-pink transition ease-in duration-700">
+          <div className="invisible group-hover:visible absolute bottom-0 left-0 w-full px-6 py-4 group-hover:bg-ruddy-pink transition ease-in duration-150">
             <h5 className=" text-xl font-bold text-white">{props.name}</h5>
             <p className="text-white opacity-70 text-sm">{props.owner}</p>
             <div className="flex items-center gap-2 mt-2">
