2c0c48b50322ffc4f8a6340b9823f672bfa5b4e8
mikael risyad <mikelrisyad@gmail.com>
2022-03-06 16:46:07 +0700

    feat: add api service room and product photos

--- services/giveaway.js
+++ services/giveaway.js
@@ -2,11 +2,33 @@ import axios from 'axios';
 
 const baseUrl = 'https://lepasaja-backend.herokuapp.com/api/v1';
 
+// crud rooms
+
 export const postRoom = async (payload, token) => {
-  await axios.post(`${baseUrl}/rooms`, payload, {
+  return axios.post(`${baseUrl}/rooms`, payload, {
     headers: {
+      method: 'POST',
       Authorization: `Bearer ${token}`,
       'Content-Type': 'multipart/form-data',
+    },
+  });
+};
+
+export const updateRoom = async (id, payload, token) => {
+  return axios.update(`${baseUrl}/rooms/${id}`, {
+    headers: {
+      method: 'UPDATE',
+      Authorization: `Bearer ${token}`,
+      body: JSON.stringify(payload),
+    },
+  });
+};
+
+export const deleteRoom = async (id, token) => {
+  return axios.delete(`${baseUrl}/rooms/${id}`, {
+    headers: {
+      method: 'DELETE',
+      Authorization: `Bearer ${token}`,
     },
   });
 };
@@ -17,8 +39,45 @@ export const getRoomById = async (id) => {
   return data;
 };
 
-export const getAllRooms = async (setState) => {
+export const getAllRooms = async () => {
   const response = await axios.get(`${baseUrl}/rooms`);
   const data = await response.data;
   return data;
+};
+
+// crud product photos
+export const getProductPhotos = async (id) => {
+  const response = await axios.get(`${baseUrl}/rooms/${id}/photos`);
+  const data = await response.data;
+  return data;
+};
+
+export const postProductPhoto = async (id, payload, token) => {
+  return axios.post(`${baseUrl}/rooms/${id}/photos`, payload, {
+    headers: {
+      method: 'POST',
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+};
+
+export const updateProductPhoto = async (id, payload, token) => {
+  return axios.post(`${baseUrl}/rooms/${id}/photos`, {
+    headers: {
+      method: 'PUT',
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'multipart/form-data',
+      body: JSON.stringify(payload),
+    },
+  });
+};
+
+export const deleteProductPhoto = async (id, token) => {
+  return axios.delete(`${baseUrl}/rooms/${id}/photos`, {
+    headers: {
+      method: 'DELETE',
+      Authorization: `Bearer ${token}`,
+    },
+  });
 };
