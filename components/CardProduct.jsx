8b848d4d5448de991b61f9294286f1af1da2849c
moehzi <faizi1234098@gmail.com>
2022-03-09 16:13:59 +0800

    fix: color in slider and show more logic

--- components/CardProduct.jsx
+++ components/CardProduct.jsx
@@ -10,14 +10,14 @@ export default function CardProduct(data) {
           <img
             src={data.src}
             alt={data.name}
-            className="object-cover w-full rounded-t-lg h-60"
+            className="object-cover w-[350px] rounded-t-lg h-60"
           />
-          <div className="invisible group-hover:visible absolute bottom-0 left-0 w-full px-6 py-4 group-hover:bg-red-600 transition ease-in duration-150">
-            <h5 className=" text-xl font-bold text-white">{data.name}</h5>
-            <p className="text-white opacity-70 text-sm">{data.owner}</p>
+          <div className="absolute bottom-0 left-0 invisible w-full px-6 py-4 transition duration-150 ease-in group-hover:visible group-hover:bg-red-600">
+            <h5 className="text-xl font-bold text-white ">{data.name}</h5>
+            <p className="text-sm text-white opacity-70">{data.owner}</p>
             <div className="flex items-center gap-2 mt-2">
               <img src="/icons/map-pin.svg" />
-              <p className="text-white text-sm">{data.location}</p>
+              <p className="text-sm text-white">{data.location}</p>
             </div>
           </div>
         </a>
