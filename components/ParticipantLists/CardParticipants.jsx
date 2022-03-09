444c49d9ca0d3af9b36c65be6e1855b68a1acc3c
moehzi <faizi1234098@gmail.com>
2022-03-09 14:52:05 +0800

    fix: fix participant list design

--- components/ParticipantLists/CardParticipants.jsx
+++ components/ParticipantLists/CardParticipants.jsx
@@ -1,11 +1,11 @@
 const CardParticipant = ({ name, imageUrl }) => {
   return (
-    <div className="flex flex-row border-2 border-gray-200 shadow-lg shadow-gray-400 rounded-lg px-3 py-5">
+    <div className="flex flex-row items-center px-3 py-3 mt-4 border-2 border-gray-200 rounded-lg shadow-lg shadow-gray-400">
       <div className="mx-1">
-        <img className="w-9/12 rounded-full" src={imageUrl} alt="" />
+        <img className="rounded-full w-[50px] h-[50px]" src={imageUrl} alt="" />
       </div>
-      <div className="mx-1">
-        <h1 className="font-bold text-md"> {name} </h1>
+      <div className="mx-2">
+        <h1 className="font-semibold text-md"> {name} </h1>
       </div>
     </div>
   );
